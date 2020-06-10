c8901cc92489717c35fd2aa705ff1cec363b8b04
Gabriele Kukauskaite <gabriele.kukauskaite@gmail.com>
2020-06-11 00:24:49 +0100

    Added player number and playtime

--- src/components/BoardgamesList/BoardgamesList.tsx
+++ src/components/BoardgamesList/BoardgamesList.tsx
@@ -1,30 +1,60 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './BoardgamesList.scss'
-import { xml2json, xml2js } from 'xml-js';
+import { xml2js } from 'xml-js';
+
+interface Boardgame {
+	id: string;
+	name?: string;
+	thumbnail?: string;
+	minPlayers?: string;
+	maxPlayers?: string;
+	minPlaytime?: string;
+	maxPlaytime?: string;
+}
 
 export default function BoardgamesList() {
 
-	const [items, setItems] = useState([]);
+	const [items, setItems] = useState<ReadonlyArray<Boardgame>>([]);
 
 	useEffect(() => {
 		fetch('https://www.boardgamegeek.com/xmlapi2/hot?type=boardgames')
 			.then(res => res.text())
 			.then(
 				(result) => {
-					const idList = xml2js(result).elements[0].elements.map(element => element.attributes.id);
+					const idList: ReadonlyArray<string> = xml2js(result).elements[0].elements.map(element => element.attributes.id);
 
-					fetch('http://127.0.0.1:8080/https://www.boardgamegeek.com/xmlapi2/thing?id=' + idList)
+					fetch('http://127.0.0.1:8080/https://www.boardgamegeek.com/xmlapi2/thing?stats=1&id=' + idList)
 						.then(res => res.text())
 						.then(
 							(result) => {
 								const resultsJs = xml2js(result);
 								console.log(resultsJs);
-								const boardgames = resultsJs.elements[0].elements.map(element => {
-									return {
-										name: element.elements[2].attributes.value,
-										thumbnail: element.elements[0].elements[0].text,
+								const boardgames = resultsJs.elements[0].elements.map(boardgame => {
+									let boardgameObj: Boardgame = {
+										id: boardgame.attributes.id,
 									}
+									boardgame.elements.forEach(boardgameProperty => {
+										if(boardgameProperty.name === 'thumbnail') {
+											boardgameObj.thumbnail = boardgameProperty.elements[0].text;
+										}
+										if(boardgameProperty.name === 'name' && boardgameProperty.attributes.type === 'primary') {
+											boardgameObj.name = boardgameProperty.attributes.value;
+										}
+										if(boardgameProperty.name === 'minplayers') {
+											boardgameObj.minPlayers = boardgameProperty.attributes.value
+										}
+										if(boardgameProperty.name === 'maxplayers') {
+											boardgameObj.maxPlayers = boardgameProperty.attributes.value
+										}
+										if(boardgameProperty.name === 'minplaytime') {
+											boardgameObj.minPlaytime = boardgameProperty.attributes.value
+										}
+										if(boardgameProperty.name === 'maxplaytime') {
+											boardgameObj.maxPlaytime = boardgameProperty.attributes.value
+										}
+									});
+									return boardgameObj;
 								})
 								setItems(boardgames);
 							},
@@ -47,7 +77,8 @@ export default function BoardgamesList() {
 						<img className='boardgame-image' alt='' src={value.thumbnail}></img>
 						<div className='boardgame-description'>
 							<div className='boardgame-name'>{value.name}</div>
-							<div className='boardgame-rank'>Rank: {value.rank}</div>
+							<div className='player-number'>{value.minPlayers} - {value.maxPlayers} players</div>
+							<div className='playtime'>{value.minPlaytime} - {value.maxPlaytime} min</div>
 						</div>
 					</div>
 				)
