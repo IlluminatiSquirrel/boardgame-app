03995d51aea8f9ebb56773542ac06c16bc9c7117
Gabriele Kukauskaite <gabriele.kukauskaite@gmail.com>
2020-06-14 15:29:12 +0100

    Refactored fetch code

--- src/components/BoardgamesList/BoardgamesList.tsx
+++ src/components/BoardgamesList/BoardgamesList.tsx
@@ -10,83 +10,92 @@ interface Boardgame {
   maxPlayers?: string;
   minPlaytime?: string;
   maxPlaytime?: string;
+  weight?: string;
+  rating?: string;
 }
 
 export default function BoardgamesList() {
   const [items, setItems] = useState<ReadonlyArray<Boardgame>>([]);
+  let idList: ReadonlyArray<string> = [];
 
   useEffect(() => {
     fetch('https://www.boardgamegeek.com/xmlapi2/hot?type=boardgames')
       .then((res) => res.text())
-      .then(
-        (hotBoardgamesResult) => {
-          const idList: ReadonlyArray<string> = xml2js(hotBoardgamesResult).elements[0].elements.map((element) => element.attributes.id);
-
-          fetch(`http://127.0.0.1:8080/https://www.boardgamegeek.com/xmlapi2/thing?stats=1&id=${idList}`)
-            .then((res) => res.text())
-            .then(
-              (boardgameDetailsResult) => {
-                const resultsJs = xml2js(boardgameDetailsResult);
-                console.log(resultsJs);
-                const boardgames = resultsJs.elements[0].elements.map(
-                  (boardgame) => {
-                    const boardgameObj: Boardgame = {
-                      id: boardgame.attributes.id,
-                    };
-                    boardgame.elements.forEach((boardgameProperty) => {
-                      if (boardgameProperty.name === 'thumbnail') {
-                        boardgameObj.thumbnail = boardgameProperty.elements[0].text;
-                      }
-                      if (boardgameProperty.name === 'name' && boardgameProperty.attributes.type === 'primary') {
-                        boardgameObj.name = boardgameProperty.attributes.value;
-                      }
-                      if (boardgameProperty.name === 'minplayers') {
-                        boardgameObj.minPlayers = boardgameProperty.attributes.value;
-                      }
-                      if (boardgameProperty.name === 'maxplayers') {
-                        boardgameObj.maxPlayers = boardgameProperty.attributes.value;
-                      }
-                      if (boardgameProperty.name === 'minplaytime') {
-                        boardgameObj.minPlaytime = boardgameProperty.attributes.value;
-                      }
-                      if (boardgameProperty.name === 'maxplaytime') {
-                        boardgameObj.maxPlaytime = boardgameProperty.attributes.value;
-                      }
-                    });
-                    return boardgameObj;
-                  },
-                );
-                setItems(boardgames);
-              },
-              (error) => {
-                console.error(error);
-              },
-            );
-        },
-        (error) => {
-          console.error(error);
-        },
-      );
+      .then((res) => {
+        idList = xml2js(res).elements[0].elements.map((element) => element.attributes.id);
+        return fetch(`http://127.0.0.1:8080/https://www.boardgamegeek.com/xmlapi2/thing?stats=1&id=${idList}`);
+      })
+      .then((res) => res.text())
+      .then((res) => parseResponse(res))
+      .catch((error) => console.error(error));
   }, []);
+
+  function parseResponse(boardgameDetailsResult) {
+    const resultsJs = xml2js(boardgameDetailsResult);
+    // console.log(resultsJs);
+    const boardgames = resultsJs.elements[0].elements.map(
+      (boardgame) => {
+        const boardgameObj: Boardgame = {
+          id: boardgame.attributes.id,
+        };
+        boardgame.elements.forEach((boardgameProperty) => {
+          if (boardgameProperty.name === 'thumbnail') {
+            boardgameObj.thumbnail = boardgameProperty.elements[0].text;
+          } else if (boardgameProperty.name === 'name' && boardgameProperty.attributes.type === 'primary') {
+            boardgameObj.name = boardgameProperty.attributes.value;
+          } else if (boardgameProperty.name === 'minplayers') {
+            boardgameObj.minPlayers = boardgameProperty.attributes.value;
+          } else if (boardgameProperty.name === 'maxplayers') {
+            boardgameObj.maxPlayers = boardgameProperty.attributes.value;
+          } else if (boardgameProperty.name === 'minplaytime') {
+            boardgameObj.minPlaytime = boardgameProperty.attributes.value;
+          } else if (boardgameProperty.name === 'maxplaytime') {
+            boardgameObj.maxPlaytime = boardgameProperty.attributes.value;
+          } else if (boardgameProperty.name === 'statistics') {
+            boardgameProperty.elements[0].elements.forEach((boardgameStat) => {
+              if (boardgameStat.name === 'average') {
+                boardgameObj.rating = boardgameStat.attributes.value;
+              } else if (boardgameStat.name === 'averageweight') {
+                boardgameObj.weight = boardgameStat.attributes.value;
+              }
+            });
+          }
+        });
+        return boardgameObj;
+      },
+    );
+    setItems(boardgames);
+  }
 
   return (
     <div>
       {items.map((value) => (
         <div key={value.id} className="boardgame-container">
-          <img className="boardgame-image" alt="" src={value.thumbnail} />
+          <img className="boardgame-image" alt="boardgame-thumbnail" src={value.thumbnail} />
           <div className="boardgame-description">
             <div className="boardgame-name">{value.name}</div>
-            <div className="player-number">
-              {value.minPlayers}
-              -
-              {value.maxPlayers}
-              players
-            </div>
-            <div className="playtime">
-              {value.minPlaytime}
-              -
-              {value.maxPlaytime}
-              min
+            <div className="boardgame-extra-details">
+              <div className="player-number">
+                {'Players: '}
+                {value.minPlayers}
+                {' - '}
+                {value.maxPlayers}
+              </div>
+              <div className="playtime">
+                {'Playtime: '}
+                {value.minPlaytime}
+                {' - '}
+                {value.maxPlaytime}
+                {' min'}
+              </div>
+              <div className="rating">
+                {'Rating: '}
+                {value.rating ? +parseFloat(value.rating).toFixed(2) : '?'}
+              </div>
+              <div className="weight">
+                {'Average weight: '}
+                {value.weight ? +parseFloat(value.weight).toFixed(2) : '?'}
+              </div>
             </div>
           </div>
         </div>
